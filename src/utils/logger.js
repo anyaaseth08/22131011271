ab1deb22e94591900d454c02f9bb876d062bdc38
anyaaseth08 <anyaaseth@gmail.com>
2025-06-27 16:27:49 +0530

    Initial Commit

--- src/utils/logger.js
+++
@@ -1,5 +0,0 @@
-export function logEvent(message) {
-  const logs = JSON.parse(localStorage.getItem("logs")) || [];
-  logs.push({ message, time: new Date().toISOString() });
-  localStorage.setItem("logs", JSON.stringify(logs));
-}
